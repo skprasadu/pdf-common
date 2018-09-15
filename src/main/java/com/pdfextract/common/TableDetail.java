c7a4abf3785e9ce7a46beb6c0ea677678e0818bb
Krishna Prasad <skprasadu@yahoo.com>
2018-09-14 20:19:58 -0700

    Fixed the tabular data

--- src/main/java/com/pdfextract/common/TableDetail.java
+++ src/main/java/com/pdfextract/common/TableDetail.java
@@ -1,5 +1,7 @@
 package com.pdfextract.common;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -45,4 +47,31 @@ public class TableDetail {
 			i++;
 		}
 	}
+	
+	public TableDetail(LinkedHashMap tableData) throws ParseException{
+
+		ArrayList tableArray = (ArrayList)tableData.get("data");
+
+		
+		ArrayList firstArr = (ArrayList) tableArray.get(0);
+		
+		columns = (List) firstArr.stream().map(x -> ((LinkedHashMap)x).get("text")).collect(Collectors.toList());
+		
+		int i = 0;
+		content =  new LinkedList<String[]>();
+		for(Object obj : tableArray){
+			ArrayList jArr = (ArrayList) obj;
+			if(i > 0){
+				String[] sts = new String[jArr.size()];
+				int j = 0;
+				for(Object obj1 : jArr){
+					LinkedHashMap jObj = (LinkedHashMap) obj1;
+					sts[j++] = (String) jObj.get("text"); 
+				}
+				content.add(sts);
+			}
+			i++;
+		}
+	}
+
 }
--- src/main/java/com/pdfextract/util/Util1.java
+++ src/main/java/com/pdfextract/util/Util1.java
@@ -1,14 +1,16 @@
 package com.pdfextract.util;
 
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
@@ -22,14 +24,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pdfextract.common.ExtractStrategy;
 import com.pdfextract.common.Layout;
 import com.pdfextract.common.Section;
+import com.pdfextract.common.TableDetail;
+import com.pdfextract.common.regex.RegexCommonUtil;
 
 import lombok.val;
 
 public class Util1 {
 
 	public static String extractCsvFromPdfExtract(InputStream in, List<String> tables, String layoutStr) {
-		// Layout layout = ExtractSections.loadYaml("ccl_layout.yaml");
-		try (val pdfDocument = PDDocument.load(in)) {
+		try (PDDocument pdfDocument = PDDocument.load(in)) {
 			return extractCsvFromPdfExtract(pdfDocument, tables, layoutStr);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -42,10 +45,9 @@ public class Util1 {
 	}
 
 	public static String extractCsvFromPdfExtract(PDDocument pdfDocument, List<String> tables, String layoutStr) {
-		// Layout layout = ExtractSections.loadYaml("ccl_layout.yaml");
-		try (val sw = new StringWriter();
-				val writer = new BufferedWriter(sw);
-				val csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);) {
+		try (StringWriter sw = new StringWriter();
+				BufferedWriter writer = new BufferedWriter(sw);
+				CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);) {
 			csvData(pdfDocument, tables, layoutStr, csvPrinter);
 			return sw.toString();
 		} catch (IOException e) {
@@ -57,8 +59,7 @@ public class Util1 {
 	}
 
 	public static String extractJsonFromPdfExtract(InputStream in, List<String> tables, String layoutStr) {
-		// Layout layout = ExtractSections.loadYaml("ccl_layout.yaml");
-		try (val pdfDocument = PDDocument.load(in)) {
+		try (PDDocument pdfDocument = PDDocument.load(in)) {
 			return extractJsonFromPdfExtract(pdfDocument, tables, layoutStr);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -71,68 +72,58 @@ public class Util1 {
 	}
 
 	public static String extractJsonFromPdfExtract(PDDocument pdfDocument, List<String> tables, String layoutStr) {
-		val arr = new JSONArray();
-		val root = new JSONObject();
+		JSONArray arr = new JSONArray();
+		JSONObject root = new JSONObject();
 		arr.add(root);
 		root.put("extraction_method", "lattice");
 		root.put("top", "506.75195");
 		root.put("left", "72.06707");
 		root.put("width", "221.339080810547");
 		root.put("height", "64.64892578125");
-		val data = jsonData(pdfDocument, tables, layoutStr);
+		JSONArray data = jsonData(pdfDocument, tables, layoutStr);
 
 		root.put("data", data);
 		root.put("spec_index", "0");
 		return arr.toJSONString();
 	}
 
-	private static String csvData(PDDocument pdfDocument, List tables, String layoutStr,
-			CSVPrinter csvPrinter) {
+	private static String csvData(PDDocument pdfDocument, List tables, String layoutStr, CSVPrinter csvPrinter) {
 		try {
 			val m = new ObjectMapper();
 			val layout = m.readValue(layoutStr, Layout.class);
-			val extractSections = (ExtractStrategy) Class
-					.forName(layout.getExtractStrategyDetails().getExtractStrategy()).newInstance();
 
 			val arrItem = new ArrayList<String>();
 
-			val sections = layout.getSections();
+			List sections = getHeaders(tables, layout);
 
-			for (val columnHeader : sections) {
-				arrItem.add(columnHeader.getName());
+			for (Object columnHeader : sections) {
+				arrItem.add((String) columnHeader);
 			}
 
 			csvPrinter.printRecord(arrItem);
 
-			val ss = extractSections.extractData(pdfDocument, layout);
-			System.out.println("ss.size()=" + ss.size() + "tables.size()=" + tables.size());
+			// get the data before RegEx
+			List<String[]> ss1 = extractData(pdfDocument, tables, layout);
+
+			// Apply RegEx IF ANY
+			val ss = RegexCommonUtil.applyRegex(ss1, 0, layout);
+
+			System.out.println("csvData ss.size()=" + ss.size() + "tables.size()=" + tables.size());
 
 			for (int i = 0; i < ss.size(); i++) {
 				val arrItem1 = new ArrayList<String>();
 				String[] row = ss.get(i);
 				for (int j = 0; j < row.length; j++) {
-					Section s = sections[j];
-					if (!s.getIsTabular()) {
-						if (row[j] != null) {
-							arrItem1.add(row[j].replace("\n", " ").replace("\r", " "));
-						} else {
-							arrItem1.add(" ");
-						}
+					if (row[j] != null && !row[j].trim().equals("")) {
+						arrItem1.add(row[j].replace("\n", " ").replace("\r", " "));
 					} else {
-						if (i < tables.size()) {
-							JSONParser parser = new JSONParser();
-							val item1 = (LinkedHashMap) tables.get(i);
-
-							arrItem1.add(getJsonDataString((ArrayList) item1.get("data")));
-						} else {
-							arrItem1.add("data");
-						}
+						arrItem1.add("no data");
 					}
 				}
 				csvPrinter.printRecord(arrItem1);
 			}
-		} catch (IOException | InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
+		} catch (IOException | InstantiationException | IllegalAccessException | ClassNotFoundException
+				| ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -143,60 +134,51 @@ public class Util1 {
 		try {
 			val m = new ObjectMapper();
 			val layout = m.readValue(layoutStr, Layout.class);
-			val extractSections = (ExtractStrategy) Class
-					.forName(layout.getExtractStrategyDetails().getExtractStrategy()).newInstance();
 
 			val data = new JSONArray();
 
 			val arrItem = new JSONArray();
 
-			val sections = layout.getSections();
+			List sections = getHeaders(tables, layout);
 
-			for (val columnHeader : sections) {
-				val header = new JSONObject();
+			for (Object columnHeader : sections) {
+				JSONObject header = new JSONObject();
 				header.put("top", "");
 				header.put("left", "");
 				header.put("width", "");
 				header.put("height", "");
-				header.put("text", columnHeader.getName());
+				header.put("text", columnHeader);
 				arrItem.add(header);
 			}
 
 			data.add(arrItem);
 
-			val ss = extractSections.extractData(pdfDocument, layout);
-			System.out.println("ss.size()=" + ss.size() + "tables.size()=" + tables.size());
+			List<String[]> ss1 = extractData(pdfDocument, tables, layout);
+			val ss = RegexCommonUtil.applyRegex(ss1, 0, layout);
+			System.out.println("jsonData ss.size()=" + ss.size() + "tables.size()=" + tables.size());
 
 			for (int i = 0; i < ss.size(); i++) {
-				val arrItem1 = new JSONArray();
-				val row = ss.get(i);
+				JSONArray arrItem1 = new JSONArray();
+				String[] row = ss.get(i);
 				for (int j = 0; j < row.length; j++) {
-					val s = sections[j];
-					val item = new JSONObject();
+					JSONObject item = new JSONObject();
 					item.put("top", "");
 					item.put("left", "");
 					item.put("width", "");
 					item.put("height", "");
-					item.put("text", row[j]);
-					if (!s.getIsTabular()) {
-						arrItem1.add(item);
+					if (row[j] != null && !row[j].trim().equals("")) {
+						item.put("text", row[j].replace("\n", " ").replace("\r", " "));
 					} else {
-						if (i < tables.size()) {
-							val parser = new JSONParser();
-							//val item1 = (JSONObject) parser.parse(tables.get(i) + "}");
-							val item1 = (LinkedHashMap) tables.get(i);
-							item.put("text", getJsonDataString((ArrayList) item1.get("data")));
-						} else {
-							item.put("text", "data");
-						}
-						arrItem1.add(item);
+						item.put("text", "no data");
 					}
+
+					arrItem1.add(item);
 				}
 				data.add(arrItem1);
 			}
 
 			return data;
-		} catch (IOException | InstantiationException | IllegalAccessException
+		} catch (IOException | ParseException | InstantiationException | IllegalAccessException
 				| ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -204,24 +186,113 @@ public class Util1 {
 		return null;
 	}
 
-	private static String getJsonDataString(ArrayList jsonArray) {
+	private static List getHeaders(List tables, Layout layout) throws ParseException {
 		// TODO Auto-generated method stub
-		val tempArray = new JSONArray();
-		int i = 0;
-		for (Object obj : jsonArray) {
-			val jArr = (ArrayList) obj;
-			val jArr1 = new JSONArray();
+		if (layout.getHeaders() != null) {
+			return Arrays.asList(layout.getHeaders());
+		} else {
+			List lst = new LinkedList();
+			List tabList = Arrays.stream(layout.getSections()).filter(x -> x.getIsTabular() == true)
+					.collect(Collectors.toList());
 
-			if (i > 0) {
-				for (Object obj1 : jArr) {
-					val jObj1 = (LinkedHashMap) obj1;
-					jArr1.add(jObj1.get("text"));
+			if (tables.size() > 0 && tabList.size() > 0) {
+				TableDetail td = new TableDetail((LinkedHashMap) tables.get(0));
+				List columns = td.getColumns();
+				for (Section s : layout.getSections()) {
+					lst.add(s.getName());
+					if (s.getIsTabular()) {
+						lst.addAll(columns);
+					}
 				}
-				tempArray.add(jArr1);
+				return lst;
+			} else {
+				return Arrays.stream(layout.getSections()).map(Section::getName).collect(Collectors.toList());
 			}
-			i++;
+		}
+	}
+
+	private static List<String[]> extractData(PDDocument pdfDocument, List tables, Layout layout)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, ParseException {
+		val extractSections = (ExtractStrategy) Class.forName(layout.getExtractStrategyDetails().getExtractStrategy())
+				.newInstance();
+		val sections = layout.getSections();
+		val ss = extractSections.extractData(pdfDocument, layout);
+		System.out.println("extractData ss.size()=" + ss.size() + "tables.size()=" + tables.size());
+
+		int count = sections.length;
+
+		if (tables.size() > 0) {
+			count = getCount((LinkedHashMap) tables.get(0), sections);
 		}
 
-		return tempArray.toJSONString();
+		val list = new LinkedList<String[]>();
+		val tabList = new LinkedList<String[]>();
+
+		for (int i = 0; i < ss.size(); i++) {
+			String[] row = ss.get(i);
+			val arrItem1 = new String[count];
+			int index = 0;
+			for (int j = 0; j < row.length; j++) {
+				Section s = sections[j];
+
+				if (row[j] != null) {
+					arrItem1[index] = row[j].replace("\n", " ").replace("\r", " ");
+				} else {
+					arrItem1[index] = " ";
+				}
+				if (s.getIsTabular()) {
+					// getJsonDataString(tables, i, arrItem1, j);
+					if (i < tables.size()) {
+						TableDetail td = new TableDetail((LinkedHashMap) tables.get(i));
+						List<String[]> contents = td.getContent();
+						int firstRow = 0;
+						int indexOfTabularData = index + 1;
+						for (String[] row1 : contents) {
+
+							if (firstRow == 0) {
+								for (int ii = 0; ii < row1.length; ii++) {
+									arrItem1[indexOfTabularData++] = row1[ii];
+								}
+								indexOfTabularData = index + 1;
+							} else {
+								val arrItem2 = new String[count];
+								for (int ii = 0; ii < row1.length; ii++) {
+									arrItem2[indexOfTabularData++] = row1[ii];
+								}
+								tabList.add(arrItem2);
+								indexOfTabularData = index + 1;
+							}
+							firstRow++;
+						}
+						index += td.getColumns().size();
+					}
+				}
+				index++;
+			}
+
+			list.add(arrItem1);
+			if (tabList.size() > 0) {
+				list.addAll(tabList);
+				tabList.clear();
+			}
+		}
+		return list;
+	}
+
+	public static int getCount(LinkedHashMap tableData, Section[] sections) throws ParseException {
+
+		TableDetail td = new TableDetail(tableData);
+
+		int cnt = td.getColumns().size();
+		int index = 0;
+
+		for (int i = 0; i < sections.length; i++) {
+			if (sections[i].getIsTabular()) {
+				index += cnt;
+			}
+			index++;
+		}
+
+		return index;
 	}
 }
